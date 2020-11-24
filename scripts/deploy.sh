d2f8fbae4239eb4f5d1e698727c82eb2af6a08ed
Kostia <kn.golovan@gmail.com>
2020-11-24 13:37:30 +0200

    fix deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 set -e
+HEROKU_API_KEY=$HEROKU_API_KEY heroku container:login
 HEROKU_API_KEY=$HEROKU_API_KEY heroku container:push    web --app setup-practice
 HEROKU_API_KEY=$HEROKU_API_KEY heroku container:release web --app setup-practice
