bb38fa9ac77ba31c90d38536c7fa96bf517eaba8
kngolovan <71441910+kngolovan@users.noreply.github.com>
2020-11-24 13:21:50 +0200

    add deploy script (#2)

---
+++ scripts/deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+set -e
+HEROKU_API_KEY=$HEROKU_API_KEY heroku container:push    web --app setup-practice
+HEROKU_API_KEY=$HEROKU_API_KEY heroku container:release web --app setup-practice
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="My Application",
-    version="1.0",
+    version="1.0.0",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
@@ -16,13 +16,13 @@ setup(
     ],
     extras_require={
         "ci": [
-            "black",
-            "coveralls",
-            "pytest",
-            "pytest-cov",
-            "pylint",
-            "pylint_flask",
-            "pylint_flask_sqlalchemy",
+            "black==20.8b1",
+            "coveralls==2.2.0",
+            "pylint==2.6.0",
+            "pylint_flask==0.6",
+            "pylint_flask_sqlalchemy==0.2.0",
+            "pytest==6.1.2",
+            "pytest-cov==2.10.1",
         ]
     },
 )
